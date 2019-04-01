4cb256efdc588e35db89f7fea61315453cec44d2
Kent C. Dodds <kent@doddsfamily.us>
2019-04-01 13:04:31 -0600

    10 checkpoint

--- src/__tests__/10.js
+++ src/__tests__/10.js
@@ -1,49 +1,15 @@
 import React from 'react'
-import {
-  findAllInRenderedTree,
-  isCompositeComponentWithType,
-} from 'react-dom/test-utils'
-import chalk from 'chalk'
 import {fireEvent, renderToggle} from '../../test/utils'
 import Usage, {Toggle} from '../exercises-final/10'
 // import Usage, {Toggle} from '../exercises/10'
 
-const findToggleInstances = rootInstance =>
-  findAllInRenderedTree(rootInstance, c =>
-    isCompositeComponentWithType(c, Toggle),
-  )
-
-function validateToggleInstance(instance) {
-  // validate the internal state of the toggle does not change
-  // If it does change then you could trigger an unnecessary re-render
-  try {
-    expect(instance.state).toEqual({on: false})
-    expect(instance.state).toEqual({on: false})
-  } catch (error) {
-    const helpfulMessage = chalk.red(
-      `🚨  The toggle should not update its own state when it's controlled  🚨`,
-    )
-    error.message = `${helpfulMessage}\n\n${error.message}`
-    throw error
-  }
-}
-
 test('toggling either toggle toggles both', () => {
-  const handleToggle = jest.fn()
-  const {getAllByTestId, rootInstance} = renderToggle(
-    <Usage onToggle={handleToggle} />,
-  )
-  const [toggleInstance1, toggleInstance2] = findToggleInstances(
-    rootInstance,
-  )
+  const {getAllByTestId} = renderToggle(<Usage />)
   const buttons = getAllByTestId('toggle-input')
   const [toggleButton1, toggleButton2] = buttons
   fireEvent.click(toggleButton1)
   expect(toggleButton1).toBeOn()
   expect(toggleButton2).toBeOn()
-
-  validateToggleInstance(toggleInstance1)
-  validateToggleInstance(toggleInstance2)
 
   fireEvent.click(toggleButton2)
   expect(toggleButton1).toBeOff()
@@ -51,15 +17,10 @@ test('toggling either toggle toggles both', () => {
 })
 
 test('toggle can still be uncontrolled', () => {
-  const handleToggle = jest.fn()
-  const {toggleButton, toggle} = renderToggle(
-    <Toggle onToggle={handleToggle} />,
-  )
+  const {toggleButton, toggle} = renderToggle(<Toggle />)
   expect(toggleButton).toBeOff()
   toggle()
   expect(toggleButton).toBeOn()
-  expect(handleToggle).toHaveBeenCalledTimes(1)
-  expect(handleToggle).toHaveBeenCalledWith(true)
 })
 
 //////// Elaboration & Feedback /////////
--- src/exercises-final/10.js
+++ src/exercises-final/10.js
@@ -3,57 +3,139 @@
 import React from 'react'
 import {Switch} from '../switch'
 
-class Toggle extends React.Component {
-  state = {on: false}
-  isControlled(prop) {
-    return this.props[prop] !== undefined
-  }
-  getState() {
-    return {
-      on: this.isControlled('on') ? this.props.on : this.state.on,
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
+const noop = () => {}
+
+function toggleReducer(state, {type, initialState}) {
+  switch (type) {
+    case useToggle.types.toggle: {
+      return {on: !state.on}
     }
-  }
-  toggle = () => {
-    if (this.isControlled('on')) {
-      this.props.onToggle(!this.getState().on)
-    } else {
-      this.setState(
-        ({on}) => ({on: !on}),
-        () => {
-          this.props.onToggle(this.getState().on)
-        },
-      )
+    case useToggle.types.reset: {
+      return initialState
     }
-  }
-  render() {
-    return <Switch on={this.getState().on} onClick={this.toggle} />
+    case useToggle.types.toggleOn: {
+      return {on: true}
+    }
+    case useToggle.types.toggleOff: {
+      return {on: false}
+    }
+    default:
+      throw new Error(`Unsupported type: ${type}`)
   }
 }
 
-class Usage extends React.Component {
-  state = {bothOn: false}
-  handleToggle = on => {
-    this.setState({bothOn: on})
+function useControlledReducer(
+  reducer,
+  initialState,
+  {externalState, onChange},
+) {
+  const [internalState, dispatch] = React.useReducer(reducer, initialState)
+  if (externalState) {
+    return [
+      externalState,
+      action => onChange(reducer(externalState, action), action),
+    ]
   }
-  render() {
-    const {bothOn} = this.state
-    const {toggle1Ref, toggle2Ref} = this.props
-    return (
+  return [internalState, dispatch]
+}
+
+function useToggle({
+  onToggle = noop,
+  onReset = noop,
+  initialOn = false,
+  reducer = toggleReducer,
+  onChange,
+  state: externalState,
+} = {}) {
+  const {current: initialState} = React.useRef({on: initialOn})
+  const [{on}, dispatch] = useControlledReducer(reducer, initialState, {
+    externalState,
+    onChange,
+  })
+
+  function toggle() {
+    const newOn = !on
+    dispatch({type: useToggle.types.toggle})
+    onToggle(newOn)
+  }
+
+  function reset() {
+    dispatch({type: useToggle.types.reset, initialState})
+    onReset(initialOn)
+  }
+
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: callAll(onClick, toggle),
+      ...props,
+    }
+  }
+
+  return {
+    on,
+    reset,
+    toggle,
+    getTogglerProps,
+  }
+}
+useToggle.reducer = toggleReducer
+useToggle.types = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
+function Toggle({on: externalOn, onChange}) {
+  let state
+  if (externalOn !== undefined) {
+    state = {on: externalOn}
+  }
+  const {on, getTogglerProps} = useToggle({state, onChange})
+  const props = getTogglerProps({on})
+  return (
+    <div>
+      <Switch {...props} />
+    </div>
+  )
+}
+
+function Usage() {
+  const [bothOn, setBothOn] = React.useState(false)
+  const [timesClicked, setTimesClicked] = React.useState(0)
+
+  function handleToggleChange(state, action) {
+    if (action.type === useToggle.types.toggle && timesClicked >= 4) {
+      return
+    }
+    setBothOn(state.on)
+    setTimesClicked(c => c + 1)
+  }
+
+  function handleResetClick(params) {
+    setBothOn(false)
+    setTimesClicked(0)
+  }
+
+  return (
+    <div>
       <div>
-        <Toggle
-          on={bothOn}
-          onToggle={this.handleToggle}
-          ref={toggle1Ref}
-        />
-        <Toggle
-          on={bothOn}
-          onToggle={this.handleToggle}
-          ref={toggle2Ref}
-        />
+        <Toggle on={bothOn} onChange={handleToggleChange} />
+        <Toggle on={bothOn} onChange={handleToggleChange} />
       </div>
-    )
-  }
+      {timesClicked > 4 ? (
+        <div data-testid="notice">
+          Whoa, you clicked too much!
+          <br />
+        </div>
+      ) : timesClicked > 0 ? (
+        <div data-testid="click-count">Click count: {timesClicked}</div>
+      ) : null}
+      <button onClick={handleResetClick}>Reset</button>
+    </div>
+  )
 }
 Usage.title = 'Control Props'
 
-export {Toggle, Usage as default}
+export default Usage
+export {Toggle}
