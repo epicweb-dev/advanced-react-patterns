fb2b49e0912ec1ac4adbe850ad1138cafb058d86
gavin <gavin.ligthart@labela.nl>
2019-05-14 14:58:56 +0200

    Add extra useEffect exercise to handle errors

---
+++ src/exercises-final/05-extra-2.js
@@ -0,0 +1,65 @@
+// Fetch data to set status
+
+import React from 'react'
+import {Switch} from '../switch'
+import mockApi from '../mockApi';
+
+const noop = () => {}
+
+function useToggle({onToggle = noop}) {
+  const [on, setOn] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+
+  React.useEffect(() => {
+    mockApi(true)
+      .then((data) => {
+        setOn(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [loading, error]);
+
+  function toggle() {
+    const newOn = !on
+    setOn(newOn)
+    onToggle(newOn)
+  }
+
+  return {
+    loading,
+    error,
+    toggle: [on, toggle],
+  };
+}
+
+function Toggle({onToggle}) {
+  const {
+    loading,
+    error,
+    toggle: [on, setOn]
+  } = useToggle(onToggle);
+
+  return (
+    <>
+      {error
+        ? <div style={{ color: 'red'}}>{error}</div>
+        : (
+          loading
+            ? <div>Loading...</div>
+            : <Switch on={on} onClick={setOn} />
+        )
+      }
+    </>
+  );
+}
+
+function Usage() {
+  return <Toggle onToggle={(...args) => console.info('onToggle', ...args)} />
+}
+Usage.title = 'Set initial state after fetching API data'
+
+export default Usage
--- src/mockApi.js
+++ src/mockApi.js
@@ -1,6 +1,11 @@
-export default () => new Promise((resolve) => {
-  setTimeout((initialOn = true) => {
-    resolve(initialOn);
-    return initialOn;
+export default (throwError = false) => new Promise((resolve, reject) => {
+  setTimeout((initialOn = true, error = 'Something went wrong while fetching the toggle state') => {
+    if(!throwError) {
+      resolve(initialOn);
+      return initialOn;
+    } else {
+      reject(error);
+      return error;
+    }
   }, 1000)
 });
