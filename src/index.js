f72d39899bbf5101e21bf1e67e8744134710415f
Kent C. Dodds <me+github@kentcdodds.com>
2020-02-24 20:12:34 -0700

    move things around a bit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './hacks'
-import 'normalize.css/normalize.css'
 import './styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MainApp from './app'
+import preval from 'preval.macro'
+import createWorkshopApp from './create-workshop-app'
+import pkg from '../package.json'
+
+const WorkshopApp = createWorkshopApp({
+  getExerciseImport: id => React.lazy(() => import(`./exercises/${id}.js`)),
+  getFinalImport: id => React.lazy(() => import(`./exercises-final/${id}.js`)),
+  getExampleImport: id => React.lazy(() => import(`./examples/${id}.js`)),
+  exerciseInfo: preval`module.exports = require('./load-exercises')()`,
+  pkg,
+})
 
 const rootEl = document.getElementById('⚛')
-ReactDOM.render(<MainApp />, rootEl)
+ReactDOM.render(<WorkshopApp />, rootEl)
