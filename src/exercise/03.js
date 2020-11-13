d990a97c6529ca8e2b8d522745fba5739b2f190a
Dale Seo <dale.seo@gmail.com>
2020-11-12 19:22:47 -0500

    Make 💰 a bit more useful (#51)

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -25,7 +25,7 @@ function Toggle({onToggle, children}) {
 // ToggleContext now
 // 🦉 You can create a helper method to retrieve the context here. Thanks to that,
 // your context won't be exposed to the user
-// 💰 `const context = useContext(ToggleContext)`
+// 💰 `const context = React.useContext(ToggleContext)`
 // 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
 function ToggleOn({on, children}) {
   return on ? children : null
