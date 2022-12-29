c645a5a0714118da95e0b65d356755e426804db9
Andrea Martinez <andrea.maort@gmail.com>
2022-12-29 17:41:38 +0100

    added togglerProps to exercise4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,11 +7,8 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
+  return {on, toggle, togglerProps}
 }
 
 function App() {
