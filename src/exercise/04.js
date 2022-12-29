7ea26a436093c711a0dbe7c5bc64c2bc3e231536
Andrea Martinez <andrea.maort@gmail.com>
2022-12-29 18:06:46 +0100

    getTogglerProps as a function added

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,17 +7,27 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  const togglerProps = {'aria-pressed': on, onClick: toggle}
-  return {on, toggle, togglerProps}
+
+  function getTogglerProps({onClick, ...props}) {
+    const togglerProps = {'aria-pressed': on, onClick: toggle, ...props}
+    return togglerProps
+  }
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
