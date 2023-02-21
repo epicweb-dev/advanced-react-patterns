f5734be4ae8ccc582c4fcc19e020ce608c1cdb8d
Kent C. Dodds <me+github@kentcdodds.com>
2023-02-21 13:44:01 +0100

    improve instructions

--- exercises/06.state-reducer/01-02.problem/app.tsx
+++ exercises/06.state-reducer/01-02.problem/app.tsx
@@ -6,15 +6,12 @@ export function App() {
 	const [timesClicked, setTimesClicked] = React.useState(0)
 	const clickedTooMuch = timesClicked >= 4
 
-	// 🐨 create a toggleStateReducer function here that accepts the state and action
-	// It should do almost the same thing the regular reducer does above except
-	// in the 'toggle' action type, it should check whether the toggle has been
-	// clicked too much and if it has then it should just return the state rather
-	// than make a new state object.
-
 	const { on, getTogglerProps, getResetterProps } = useToggle({
-		// 🐨 Pass your toggleStateReducer as the `reducer` option
-		// 💰 reducer: toggleStateReducer,
+		// 🐨 create a reducer function here that accepts the state and action
+		// It should do almost the same thing the regular reducer does in
+		// ./toggle.tsx except in the action.type === 'toggle' case, it should check
+		// whether the toggle has been clicked too much and if it has then it should
+		// just return the state rather than make a new state object.
 	})
 
 	return (
