3b60081e050a7b65bb920bf7737285e54ca153f9
Jason Ruesch <jruesch@degreed.com>
2024-04-10 15:03:40 -0600

    feat(slot-prop): add slot type for TypeScript enforcement

--- exercises/04.slots/03.solution.prop/slots.tsx
+++ exercises/04.slots/03.solution.prop/slots.tsx
@@ -1,13 +1,20 @@
 import { createContext, use } from 'react'
 import { Switch as BaseSwitch } from '#shared/switch'
 
+export type SlotType =
+	| 'label'
+	| 'input'
+	| 'switch'
+	| 'text'
+	| 'onText'
+	| 'offText'
 type Slots = Record<string, Record<string, unknown>>
 export const SlotContext = createContext<Slots>({})
 
 function useSlotProps<Props>(
-	props: Props & { slot?: string },
-	defaultSlot?: string,
-): Props {
+	props: Props & { slot?: SlotType },
+	defaultSlot?: SlotType,
+): Props & { slot?: SlotType } {
 	const slot = props.slot ?? defaultSlot
 	if (!slot) return props
 
@@ -16,30 +23,32 @@ function useSlotProps<Props>(
 	// a more proper "mergeProps" function is in order here
 	// to handle things like merging event handlers better.
 	// we'll get to that a bit in a later exercise.
-	return { ...slots[slot], slot, ...props } as Props
+	return { ...slots[slot], slot, ...props } as Props & { slot?: SlotType }
 }
 
 export function Label(
-	props: React.ComponentProps<'label'> & { slot?: string },
+	props: React.ComponentProps<'label'> & { slot?: SlotType },
 ) {
 	props = useSlotProps(props, 'label')
 	return <label {...props} />
 }
 
 export function Input(
-	props: React.ComponentProps<'input'> & { slot?: string },
+	props: React.ComponentProps<'input'> & { slot?: SlotType },
 ) {
 	props = useSlotProps(props, 'input')
 	return <input {...props} />
 }
 
-export function Text(props: React.ComponentProps<'span'> & { slot?: string }) {
+export function Text(
+	props: React.ComponentProps<'span'> & { slot?: SlotType },
+) {
 	props = useSlotProps(props, 'text')
 	return <span {...props} />
 }
 
 type SwitchProps = Omit<React.ComponentProps<typeof BaseSwitch>, 'on'> & {
-	slot?: string
+	slot?: SlotType
 }
 export function Switch(props: SwitchProps) {
 	return (
