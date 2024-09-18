d24b765fb4779959c75c4e4d824332b5e06c2849
Kent C. Dodds <me+github@kentcdodds.com>
2024-09-18 16:51:28 -0600

    make it easier to run tests

---
+++ epicshop/playwright.config.js
@@ -0,0 +1,41 @@
+import os from 'os'
+import path from 'path'
+import { defineConfig, devices } from '@playwright/test'
+
+const PORT = process.env.PORT || '5639'
+const tmpDir = path.join(
+	os.tmpdir(),
+	'epicshop-playwright',
+	path.basename(new URL('../', import.meta.url).pathname),
+)
+
+export default defineConfig({
+	workers: process.env.CI ? 1 : undefined,
+	outputDir: path.join(tmpDir, 'playwright-test-output'),
+	reporter: [
+		[
+			'html',
+			{ open: 'never', outputFolder: path.join(tmpDir, 'playwright-report') },
+		],
+	],
+	use: {
+		baseURL: `http://localhost:${PORT}/`,
+		trace: 'retain-on-failure',
+	},
+
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
+	],
+
+	webServer: {
+		command: 'cd .. && npm start',
+		port: Number(PORT),
+		reuseExistingServer: !process.env.CI,
+		stdout: 'pipe',
+		stderr: 'pipe',
+		env: { PORT },
+	},
+})
